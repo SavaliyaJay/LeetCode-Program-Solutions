8547dc5e6fc803a45a188fde27d366bf4ac4a900
Jay Savaliya <102573818+SavaliyaJay@users.noreply.github.com>
2024-09-07 14:42:06 +0530

    Time: 9 ms (15.89%), Space: 23.6 MB (10.81%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,32 +1,54 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         string res,data;
+//         int flag = 0;
+        
+//         vector<string> v;
+        
+//         s+= ' ';
+        
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i] == ' '){
+//                 if (!res.empty()) {
+//                     v.push_back(res);
+//                     res = "";
+//                 }
+//             }else{
+//                 res+=s[i];
+//             }
+//         }
+        
+//         for(int i=v.size()-1; i >= 0; i--){
+//             data = data + v[i];
+            
+//             if(i != 0){
+//                 data += ' ';
+//             }
+//         }
+        
+//         return data;
+//     }
+// };
+
 class Solution {
 public:
     string reverseWords(string s) {
         string res,data;
-        int flag = 0;
         
-        vector<string> v;
-        
-        s+= ' ';
-        
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == ' '){
-                if (!res.empty()) {
-                    v.push_back(res);
+        s+=' ';
+         for(auto s1 : s){
+             if(s1 == ' '){
+                 if (!res.empty()) {
+                    data = res + ' ' + data;
                     res = "";
-                }
-            }else{
-                res+=s[i];
-            }
-        }
+                 }
+             }else{                 
+                 res += s1;
+             }          
+         }
         
-        for(int i=v.size()-1; i >= 0; i--){
-            data = data + v[i];
-            
-            if(i != 0){
-                data += ' ';
-            }
-        }
-        
+        data.erase(data.size()-1,1);
         return data;
     }
 };
