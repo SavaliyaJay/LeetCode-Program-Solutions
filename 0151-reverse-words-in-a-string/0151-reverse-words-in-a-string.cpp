f75103310919d8a55354fc91c4ecbac11a201583
Jay Savaliya <102573818+SavaliyaJay@users.noreply.github.com>
2024-09-07 14:42:18 +0530

    Time: 9 ms (15.89%), Space: 23.4 MB (23.13%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,36 +1,3 @@
-// class Solution {
-// public:
-//     string reverseWords(string s) {
-//         string res,data;
-//         int flag = 0;
-        
-//         vector<string> v;
-        
-//         s+= ' ';
-        
-//         for(int i=0; i<s.size(); i++){
-//             if(s[i] == ' '){
-//                 if (!res.empty()) {
-//                     v.push_back(res);
-//                     res = "";
-//                 }
-//             }else{
-//                 res+=s[i];
-//             }
-//         }
-        
-//         for(int i=v.size()-1; i >= 0; i--){
-//             data = data + v[i];
-            
-//             if(i != 0){
-//                 data += ' ';
-//             }
-//         }
-        
-//         return data;
-//     }
-// };
-
 class Solution {
 public:
     string reverseWords(string s) {
