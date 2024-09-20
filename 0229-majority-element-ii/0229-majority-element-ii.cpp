db12302abd0e670492a18f6c32ffacaa707c9b9b
Jay Savaliya <102573818+SavaliyaJay@users.noreply.github.com>
2024-09-20 22:01:46 +0530

    Time: 3 ms (98.26%), Space: 18.7 MB (55.22%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int d = nums.size() / 3;
+        
+        cout<<d;
+        
+        map<int,int> m;
+        
+        for(auto n: nums){
+            m[n]++;
+        }
+        
+        vector<int> d1;
+        
+        for(auto m1: m){
+            if(m1.second > d){
+                d1.push_back(m1.first);
+            }
+        }
+        
+        return d1;
+    }
+};
